6c6bf60e800bc6c51859764980ffe43c29ecaddc
wanglong jing <jwl@wanglongs-MBP.lan>
2023-08-27 23:01:06 +0800

    feat: add health check interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let ln = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind");
+    let ln = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind");
+    zero2prod::run(ln)?.await
 }
