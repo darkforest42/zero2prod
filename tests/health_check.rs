15fb01b187932f30c259d19c31f35fde29822bd1
wanglong jing <jwl@wanglongs-MBP.lan>
2023-08-27 23:03:38 +0800

    test: a unit test for health check

---
+++ tests/health_check.rs
@@ -0,0 +1,25 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &addr))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let ln = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = ln.local_addr().unwrap().port();
+    let server = zero2prod::run(ln).expect("Faild to bind address");
+
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
