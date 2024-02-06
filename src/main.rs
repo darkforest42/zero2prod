8364b469856023318a1abf7c57c61ba90171f175
Jing <mechamigo@gmail.com>
2024-02-06 17:56:37 +0800

    feat: init pq db

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,5 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let ln = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind");
-    let ln = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind");
     zero2prod::run(ln)?.await
 }
